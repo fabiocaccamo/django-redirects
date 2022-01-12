ab086a61e161b506906202b64cf040c6d77fcc9b
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-01-12 02:22:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,25 +5,30 @@ from setuptools import find_packages, setup
 
 import os, sys
 
-exec(open('redirects/metadata.py').read())
+exec(open("redirects/metadata.py").read())
 
-github_url = 'https://github.com/fabiocaccamo'
-package_name = 'django-redirects'
-package_url = '{}/{}'.format(github_url, package_name)
+github_url = "https://github.com/fabiocaccamo"
+sponsor_url = "https://github.com/sponsors/fabiocaccamo/"
+twitter_url = "https://twitter.com/fabiocaccamo"
+package_name = "django-redirects"
+package_url = "{}/{}".format(github_url, package_name)
+package_issues_url = "{}/issues".format(github_url)
 package_path = os.path.abspath(os.path.dirname(__file__))
-long_description_file_path = os.path.join(package_path, 'README.md')
-long_description_content_type = 'text/markdown'
-long_description = ''
+long_description_file_path = os.path.join(package_path, "README.md")
+long_description_content_type = "text/markdown"
+long_description = ""
 try:
-    long_description_file_options = {} if sys.version_info[0] < 3 else { 'encoding':'utf-8' }
-    with open(long_description_file_path, 'r', **long_description_file_options) as f:
+    long_description_file_options = (
+        {} if sys.version_info[0] < 3 else {"encoding": "utf-8"}
+    )
+    with open(long_description_file_path, "r", **long_description_file_options) as f:
         long_description = f.read()
 except IOError:
     pass
 
 setup(
     name=package_name,
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     include_package_data=True,
     version=__version__,
     description=__description__,
@@ -32,45 +37,63 @@ setup(
     author=__author__,
     author_email=__email__,
     url=package_url,
-    download_url='{}/archive/{}.tar.gz'.format(package_url, __version__),
+    download_url="{}/archive/{}.tar.gz".format(package_url, __version__),
+    project_urls={
+        "Documentation": package_url,
+        "Issues": package_issues_url,
+        "Funding": sponsor_url,
+        "Twitter": twitter_url,
+    },
     keywords=[
-        'django', 'admin', 'redirects', 'redirect', 'permanent', 'temporary',
-        '301', '302', '303', '307', '308', 'exact', 'prefix', 'regex',
+        "django",
+        "admin",
+        "redirects",
+        "redirect",
+        "permanent",
+        "temporary",
+        "301",
+        "302",
+        "303",
+        "307",
+        "308",
+        "exact",
+        "prefix",
+        "regex",
     ],
-    requires=['django(>=1.7)'],
+    requires=["django(>=1.7)"],
     install_requires=[
-        'six >= 1.9.0, < 2.0.0',
+        "six >= 1.9.0, < 2.0.0",
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.0',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development :: Build Tools',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Build Tools",
     ],
     license=__license__,
-    test_suite='runtests.runtests'
+    test_suite="runtests.runtests",
 )
