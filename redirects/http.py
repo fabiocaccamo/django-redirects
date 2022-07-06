e036c8dbb57d37260b17fda6d5ce9f0c27df0756
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-07-07 00:47:32 +0200

    Sorted HTTP response classes.

--- redirects/http.py
+++ redirects/http.py
@@ -8,13 +8,13 @@ from django.http import (
 from django.http.response import HttpResponseRedirectBase
 
 
-class HttpResponseStrictPermanentRedirect(HttpResponseRedirectBase):
-    status_code = 308
-
-
 class HttpResponseSeeOtherRedirect(HttpResponseRedirectBase):
     status_code = 303
 
 
 class HttpResponseStrictTemporaryRedirect(HttpResponseRedirectBase):
     status_code = 307
+
+
+class HttpResponseStrictPermanentRedirect(HttpResponseRedirectBase):
+    status_code = 308
