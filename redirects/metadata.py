68c914c706f0afa50e08adafa8380971eb5069f5
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-12-13 23:45:47 +0100

    Update copyright date.

--- redirects/metadata.py
+++ redirects/metadata.py
@@ -1,5 +1,5 @@
 __author__ = "Fabio Caccamo"
-__copyright__ = "Copyright (c) 2020 Fabio Caccamo"
+__copyright__ = "Copyright (c) 2020-present Fabio Caccamo"
 __description__ = "django redirects as they should be, with full control."
 __email__ = "fabio.caccamo@gmail.com"
 __license__ = "MIT"
